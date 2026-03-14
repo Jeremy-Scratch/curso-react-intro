bb007ed9a8eec6eafd0d6152e71f40abff3c95da
Scratch <Jeremyscratch6@gmail.com>
2026-03-14 19:17:10 -0400

    ADD: logic to open and close the modal

--- src/components/TodoCreateButton.js
+++ src/components/TodoCreateButton.js
@@ -1,12 +1,16 @@
+import React from "react";
 import { MdAddCircleOutline } from "react-icons/md";
+import { TodoContext } from "../todoContext";
 
 function CreateTodoButton() {
+  const { setOpenModal}= React.useContext(TodoContext);
   return (
-    <span className="AddIcon onClickSkew" >
+    <span
+    onClick={() => setOpenModal( openModal => !openModal)} 
+    className="AddIcon onClickSkew" >
        <MdAddCircleOutline style={{height: "30px",width: "30px"}} />
     </span>
      
-    
   );
 }
 export { CreateTodoButton };
